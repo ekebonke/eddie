8393f8452f355e8e5e253edef8e7d368ddeebb5e
Ekebonke <ekebonke@gmail.com>
2013-09-11 19:41:34 +0200

    figured out the typeclass syntax

--- src/Eddie/Search.hs
+++ src/Eddie/Search.hs
@@ -1,2 +1,22 @@
-module Eddie.Gen where
+module Eddie.Search where
 
+type Pred s = s -> Bool
+
+data Node s a = 
+   InitialNode { state :: s } |
+   Node {
+       parent :: Node s a,
+       state :: s,
+       action :: a,
+       depth :: Int,
+       cost :: Double } deriving (Show, Eq)
+
+class Problem p where
+    makeNode :: p -> Node s a -> a -> Node s a
+    stepCost :: p -> Node s a -> a -> Double
+    actions :: p -> s -> [a]
+
+
+search :: (Problem p) => p -> s -> Pred s -> Maybe (Node s a)
+search problem initialState goalTest = 
+    Just $ InitialNode initialState
